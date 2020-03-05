96444a6141c345fa8551dbf113de48bd1c997829
Luiz <luiz.fss@hotmail.com>
2020-03-05 17:23:29 -0300

    Aplicativo : Calculadora de IMC

---
+++ lib/Home.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados";
+
+  void _resetFields() {
+    setState(() {
+      _infoText = "";
+    });
+    weightController.text = "";
+    heightController.text = "";
+  }
+
+  void calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc > 40.0) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+    print(_infoText);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                      Icons.person_outline,
+                      size: 120.0,
+                      color: Colors.green),
+                  TextFormField(
+                    controller: weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Peso em (kg)",
+                      labelStyle: TextStyle(
+                          color: Colors.green
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 25.0
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira o peso";
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    controller: heightController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Altura em (cm)",
+                      labelStyle: TextStyle(
+                          color: Colors.green
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 25.0),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira sua altura";
+                      }
+                    },
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Container(
+                        height: 50.0,
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              calculate();
+                            }
+                          },
+                          child: Text("Calcular",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25.0
+                            ),
+                          ),
+                            color: Colors.green,
+                        ),
+
+                      )
+                  ),
+                  Text(_infoText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 25.0
+                    ),
+                  )
+                ],
+              ),
+            )
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:calculadora_imc/Home.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
